036ac0827fa995a823a0eb4503e1d4ab0355c70c
Vision295 <theophilewacthel@gmail.com>
2025-03-24 16:33:50 +0100

    transfert en orienté objet (ne fonctionne pas encore)

--- Code/subnetsGen.py
+++ Code/subnetsGen.py
@@ -5,21 +5,23 @@ class SubnetsGen:
 
       def __init__(self, intent: dict):
             self.intent = intent
+            self.subnet_dict = {}
+            self.subnet_interconnexion_dict = {}
 
       def give_subnet_dict(self) -> dict:
             """
             Creates a dict associating a unique number to every physical link in the network.
             Args:
-            topology (dict): The network topology.
+            self.intent (dict): The network self.intent.
             Returns:
             dict: A dictionary with unique numbers for each physical link.
             Example:
             {'AS_1': {('R1', 'R2'): 1, ('R1', 'R3'): 2, ... }}
             """
-            subnet_dict = {}
+            self.subnet_dict = {}
             # Iterate over each AS
             for AS in self.intent :
-                  subnet_dict[AS] = {}
+                  self.subnet_dict[AS] = {}
                   subnet_number = 1
                   # Iterate over each router of the current AS
                   for router in self.intent[AS]['routers'] :
@@ -27,11 +29,10 @@ class SubnetsGen:
                         for neighbor in self.intent[AS]['routers'][router] :
                               # To avoid duplicates, ensure the router with the smaller numeric suffix comes first
                               if router[1:] < neighbor[1:] :
-                                    subnet_dict[AS][(router, neighbor)] = subnet_number
+                                    self.subnet_dict[AS][(router, neighbor)] = subnet_number
                                     subnet_number += 1
-            return subnet_dict
 
-      def last_entries_subnet(self, subnet_dict: dict) -> int:
+      def last_entries_subnet(self) -> None:
             """
             Find and return the last subnet value used.
             Args:
@@ -40,45 +41,43 @@ class SubnetsGen:
             int: The last subnet value used.
             """
             last_entry = dict()
-            for AS in subnet_dict:
-                  last_entry[AS] = list(subnet_dict[AS].values())[-1]
+            for AS in self.subnet_dict:
+                  last_entry[AS] = list(self.subnet_dict[AS].values())[-1]
             return last_entry
 
-      def give_subnet_interconnexion(topology: dict, subnet_dict: dict) -> dict:
+      def give_subnet_interconnexion(self) -> None:
           """
           Generates a dict of subnets for the inter-AS connections.
           Args:
-              topology (dict): The network topology.
+              self.intent (dict): The network self.intent.
               subnet_dict (dict): The subnet dictionary.
           Returns:
               dict: A dictionary of subnets for inter-AS connections.
           """
           # Create necessary dict
-          subnet_interconnexion_dict = dict()
-          last_entries = self.last_entries_subnet(subnet_dict)
-          for AS in topology:
-              subnet_interconnexion_dict[AS] = dict()
+          last_entries = self.last_entries_subnet(self.subnet_dict)
+          for AS in self.intent:
+              self.subnet_interconnexion_dict[AS] = dict()
           # Iterate over each AS
-          for AS in topology:
+          for AS in self.intent:
               # Iterate over each AS neighbor
-              for AS_neighbor in topology[AS]['neighbor']:
+              for AS_neighbor in self.intent[AS]['neighbor']:
                   # Iterate over each border Router
-                  for router1 in topology[AS]['neighbor'][AS_neighbor]:
+                  for router1 in self.intent[AS]['neighbor'][AS_neighbor]:
                       # Iterate over each neighbor Router
-                      for router2 in topology[AS]['neighbor'][AS_neighbor][router1]:
+                      for router2 in self.intent[AS]['neighbor'][AS_neighbor][router1]:
                           # Check externality of the link
-                          if (router1, router2) not in subnet_interconnexion_dict[AS].keys() and (router2, router1) not in subnet_interconnexion_dict[AS].keys():
+                          if (router1, router2) not in self.subnet_interconnexion_dict[AS].keys() and (router2, router1) not in self.subnet_interconnexion_dict[AS].keys():
                               # Create the end of the address of each externnal interface of borderRouters (ex. 111::1 -> AS 1, link 11, router 1)
                               if AS < AS_neighbor:
-                                  subnet_interconnexion_dict[AS][(router1, router2)] = str(int(AS[3:])) + str((last_entries[AS] + 1)) + "::1"
-                                  subnet_interconnexion_dict[AS_neighbor][(router2, router1)] = str(int(AS[3:])) + str((last_entries[AS] + 1)) + "::2"
+                                  self.subnet_interconnexion_dict[AS][(router1, router2)] = str(int(AS[3:])) + str((last_entries[AS] + 1)) + "::1"
+                                  self.subnet_interconnexion_dict[AS_neighbor][(router2, router1)] = str(int(AS[3:])) + str((last_entries[AS] + 1)) + "::2"
                               else:
-                                  subnet_interconnexion_dict[AS][(router1, router2)] = str(int(AS_neighbor[3:])) + str((last_entries[AS] + 1)) + "::1"
-                                  subnet_interconnexion_dict[AS_neighbor][(router2, router1)] = str(int(AS_neighbor[3:])) + str((last_entries[AS] + 1)) + "::2"
+                                  self.subnet_interconnexion_dict[AS][(router1, router2)] = str(int(AS_neighbor[3:])) + str((last_entries[AS] + 1)) + "::1"
+                                  self.subnet_interconnexion_dict[AS_neighbor][(router2, router1)] = str(int(AS_neighbor[3:])) + str((last_entries[AS] + 1)) + "::2"
                               last_entries[AS] += 1
-          return subnet_interconnexion_dict
 
-      def get_subnet_interconnexion(AS: str, subnet_interconnexion_dict: dict, routeur1: str, routeur2: str) -> str:
+      def get_subnet_interconnexion(self, AS: str, routeur1: str, routeur2: str) -> str:
           """
           Retrieve the IPv6 address of an interconnection subnet between two given routers.
           Args:
@@ -89,13 +88,13 @@ class SubnetsGen:
           Returns:
               str: The IPv6 address of the interconnection subnet.
           """
-          return subnet_interconnexion_dict[AS][(routeur1, routeur2)] or subnet_interconnexion_dict[AS][(routeur2, routeur1)]
+          return self.subnet_interconnexion_dict[AS][(routeur1, routeur2)] or self.subnet_interconnexion_dict[AS][(routeur2, routeur1)]
 
-      def generate_addresses_dict(topology: dict) -> dict:
+      def generate_addresses_dict(self) -> None:
           """
           Generates a dictionary with neighbors, interfaces, IP addresses, and AS for each router.
           Args:
-              topology (dict): The network topology.
+              self.intent (dict): The network self.intent.
           Returns:
               dict: A dictionary with neighbors, interfaces, IP addresses, and AS for each router.
           Output format:
@@ -107,16 +106,16 @@ class SubnetsGen:
           """
           router_neighbors = {}
           # Creates the subnet_dict
-          subnet_dict = give_subnet_dict(topology)
-          subnet_interconnexion_dict = give_subnet_interconnexion(topology, subnet_dict)
+          subnet_dict = self.give_subnet_dict()
+          subnet_interconnexion_dict = self.give_subnet_interconnexion()
           # Iterate over each AS
-          for AS in topology:
+          for AS in self.intent:
               # Iterate over each router in the current AS
-              for router in topology[AS]['routers']:
+              for router in self.intent[AS]['routers']:
                   if router not in router_neighbors:
                       router_neighbors[router] = []
                   # Iterate over each neighbor of the current router
-                  for neighbor, interface in topology[AS]['routers'][router].items():
+                  for neighbor, interface in self.intent[AS]['routers'][router].items():
                       # To ensure it's in the correct order
                       if router[1:] < neighbor[1:]:
                           subnet_index = subnet_dict[AS][(router, neighbor)]
@@ -124,13 +123,13 @@ class SubnetsGen:
                       else:
                           subnet_index = subnet_dict[AS][(neighbor, router)]
                           router_index = 2
-                      ipv6_address = f"{topology[AS]['address']}{subnet_index}::{router_index}{topology[AS]['subnet_mask']}"
+                      ipv6_address = f"{self.intent[AS]['address']}{subnet_index}::{router_index}{self.intent[AS]['subnet_mask']}"
                       router_neighbors[router].append({neighbor: [interface, ipv6_address, AS]})
           # Handle inter-AS connections
-          for AS in topology:
-              for AS_neighbor in topology[AS]['neighbor']:
-                  for router1 in topology[AS]['neighbor'][AS_neighbor]:
-                      for router2, interface in topology[AS]['neighbor'][AS_neighbor][router1].items():
+          for AS in self.intent:
+              for AS_neighbor in self.intent[AS]['neighbor']:
+                  for router1 in self.intent[AS]['neighbor'][AS_neighbor]:
+                      for router2, interface in self.intent[AS]['neighbor'][AS_neighbor][router1].items():
                           if router1 not in router_neighbors:
                               router_neighbors[router1] = []
                           if router2 not in router_neighbors:
@@ -139,11 +138,11 @@ class SubnetsGen:
                           # Check if the connection already exists to avoid duplication
                           if not any(neighbor.get(router2) for neighbor in router_neighbors[router1]):
                               if int(AS[3:]) < int(AS_neighbor[3:]):
-                                  ipv6_address1 = f"{topology[AS]['address'][:-1]}{get_subnet_interconnexion(AS, subnet_interconnexion_dict, router1, router2)}{topology[AS]['subnet_mask']}"
-                                  ipv6_address2 = f"{topology[AS]['address'][:-1]}{get_subnet_interconnexion(AS_neighbor, subnet_interconnexion_dict, router2, router1)}{topology[AS]['subnet_mask']}"
+                                  ipv6_address1 = f"{self.intent[AS]['address'][:-1]}{self.get_subnet_interconnexion(AS, router1, router2)}{self.intent[AS]['subnet_mask']}"
+                                  ipv6_address2 = f"{self.intent[AS]['address'][:-1]}{self.get_subnet_interconnexion(AS_neighbor, router2, router1)}{self.intent[AS]['subnet_mask']}"
                               else:
-                                  ipv6_address1 = f"{topology[AS]['address'][:-1]}{get_subnet_interconnexion(AS_neighbor, subnet_interconnexion_dict, router2, router1)}{topology[AS]['subnet_mask']}"
-                                  ipv6_address2 = f"{topology[AS]['address'][:-1]}{get_subnet_interconnexion(AS, subnet_interconnexion_dict, router1, router2)}{topology[AS]['subnet_mask']}"
+                                  ipv6_address1 = f"{self.intent[AS]['address'][:-1]}{self.get_subnet_interconnexion(AS_neighbor, router2, router1)}{self.intent[AS]['subnet_mask']}"
+                                  ipv6_address2 = f"{self.intent[AS]['address'][:-1]}{self.get_subnet_interconnexion(AS, router1, router2)}{self.intent[AS]['subnet_mask']}"
 
                               router_neighbors[router1].append({router2: [interface, ipv6_address1, AS_neighbor]})
                               router_neighbors[router2].append({router1: [interface, ipv6_address2, AS]})
