76b795cf5c547e86ec2d96f5e210f3fb55f525ee
Vision295 <theophilewacthel@gmail.com>
2025-03-24 16:23:44 +0100

    ajout des fonctions relatives à la créations de sous réseaux sous la class subnetGen

--- Code/router.py
+++ Code/router.py
@@ -4,12 +4,16 @@ from tools import *
 class Router:
       
       
-      def __init__(self):
-            ...
+      def __init__(self, intent:dict):
+            """
+            content = contenu du fichier de configuration
+            """
+            self.content = ""
+            self.intent = intent
             
-            
+      def generate_base(self):
+           ... 
       
-
 
 
 
