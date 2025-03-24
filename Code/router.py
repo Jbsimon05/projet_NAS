92d555a4de0bd17e124b52b02ee33be559eab247
Jean-Baptiste SIMON <72874867+Jbsimon05@users.noreply.github.com>
2025-03-24 17:15:43 +0100

    up router

--- Code/router.py
+++ Code/router.py
@@ -1,23 +1,12 @@
 from tools import *
 
-
 class Router:
-      
-      
-      def __init__(self, intent:dict):
-            """
-            content = contenu du fichier de configuration
-            """
-            self.content = ""
-            self.intent = intent
-            
-      def generate_base(self):
-           ... 
-      
+    def __init__(self, intent:dict):
+        """
+        content = contenu du fichier de configuration
+        """
+        self.content = ""
+        self.intent = intent
 
-
-
-
-
-
-
+    def generate_base(self):
+        ...
