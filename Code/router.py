b72cde18280d1a05b15b5ce3d2cbcea8ae22cd0c
Jean-Baptiste SIMON <72874867+Jbsimon05@users.noreply.github.com>
2025-04-01 17:24:02 +0200

    up router avec config interfaces

--- Code/router.py
+++ Code/router.py
@@ -1,8 +1,8 @@
-from tools import get_subnet, get_reversed_mask
 from template import *
+from tools import insert_line, find_index, get_subnet, get_reversed_mask, get_ip_and_mask
 
 class Router:
-    
+
     def __init__(self, router_name:str, intent:dict, subnets:dict):
         """
         content = contenu du fichier de configuration
@@ -11,17 +11,25 @@ class Router:
         self.router_name = router_name
         self.intent = intent
         self.subnets = subnets
-    
+
     def generate_init_config(self):
         self.file += INIT_CONFIG(self.router_name)
-
+        self.file += "!\n"
 
     def generate_interfaces(self):
         """
         Génère la configuration des interfaces pour le routeur.
         """
-        ...
-        
+        for interface, specs in self.subnets[self.router_name].items():
+            ip, mask = get_ip_and_mask(specs["IP"])
+            self.file += f"interface {interface}\n"
+            self.file += f" ip address {ip} {mask}\n"
+            if "duplex" in specs:
+                self.file += f" duplex {specs['duplex']}\n"
+            if "mpls" in specs and specs["mpls"]:
+                self.file += " mpls ip\n"
+            self.file += "!\n"
+
     def generate_igp(self):
         self.file += "router ospf 1\n"
 
@@ -31,7 +39,6 @@ class Router:
                 get_reversed_mask(self.subnets[self.router_name][neighbor.keys()][1])
             )
 
-        
     def generate_finale_config(self):
         self.file += FINAL_CONFIG
 
